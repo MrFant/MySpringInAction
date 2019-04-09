b7ea5db997124b41de6c10327f3d5ad36895c0fa
Mr.fant <yii.fant@gmail.com>
2019-04-09 10:12:04 +0800

    给第三章添加settings.gradle，外部注入模块添加占位符配置

--- Chapter_03/externals/src/test/java/com/soundsystem/EnvironmentConfig.java
+++ Chapter_03/externals/src/test/java/com/soundsystem/EnvironmentConfig.java
@@ -1,10 +1,14 @@
 package com.soundsystem;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.env.Environment;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 
 @Configuration
 @PropertySource("classpath:/com/soundsystem/app.properties")
@@ -12,12 +16,30 @@ public class EnvironmentConfig {
 
   @Autowired
   Environment env;
-  
+
+  @Value("${disc.title}")
+  String title;
+
+  @Value("${disc.artist}")
+  String artist;
+
   @Bean
   public BlankDisc blankDisc() {
     return new BlankDisc(
         env.getProperty("disc.title"),
         env.getProperty("disc.artist"));
+//    return new BlankDisc(this.title,this.artist);
   }
-  
+
+
+
+  @Bean
+  // 这里一定要是static 方法，否则会出错
+  public static PropertySourcesPlaceholderConfigurer properties() {
+    PropertySourcesPlaceholderConfigurer pspc = new PropertySourcesPlaceholderConfigurer();
+//    Resource[] resources = new ClassPathResource[]{new ClassPathResource("/com/soundsystem/app.properties")};
+//    pspc.setLocations(resources);
+//    pspc.setIgnoreUnresolvablePlaceholders(true);
+    return pspc;
+  }
 }
