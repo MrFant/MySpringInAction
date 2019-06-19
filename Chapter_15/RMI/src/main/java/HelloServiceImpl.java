8edebc26a8c82a5fe4b7cc90cd49bb0876ed6a0b
Mr.fant <yii.fant@gmail.com>
2019-06-19 23:30:41 +0800

    fix alert,add code,fix gitignore

---
+++ Chapter_15/RMI/src/main/java/HelloServiceImpl.java
@@ -0,0 +1,2 @@
+public class HelloServiceImpl {
+}
---
+++ Chapter_15/RMI/src/main/java/RMIServer.java
@@ -0,0 +1,23 @@
+import java.rmi.AlreadyBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
+public class Server {
+    public static void main(String[] args) {
+        try {
+            HelloServiceImpl hello=new HelloServiceImpl();
+            HelloService stub=(HelloService) UnicastRemoteObject.exportObject(hello, 9999);
+            LocateRegistry.createRegistry(1099);
+            Registry registry=LocateRegistry.getRegistry();
+            registry.bind("helloword", stub);
+            System.out.println("绑定成功!");
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        } catch (AlreadyBoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
